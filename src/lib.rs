e9515bee0e6de5daf66fd1b006e7292a18b4f1cc
Peter Kehl <peter.kehl@gmail.com>
2023-05-18 23:23:01 -0700

    Outer macro accepts path, then passes that as tt.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,24 @@
 macro_rules! prefixed_lint_versioned {
-    ($major_minor:tt, $lint_path:tt) => {
-    }
+    ($major_minor:tt, $lint_path:tt) => {};
 }
 
 macro_rules! consume_path_only {
-    ($lint_path:tt) => {
-    }
+    ($lint_path:tt) => {};
+}
+
+macro_rules! consume_path_only_as_path_then_pass_down_as_tt {
+    ($lint_path:path) => {};
+}
+
+macro_rules! consume_tt_only {
+    ($lint_path:tt) => {};
 }
 
 pub fn test_macro() {
+    // The following two fail to compile.
     prefixed_lint_versioned!(1.2, clippy::alloc_instead_of_core);
-
     consume_path_only!(clippy::alloc_instead_of_core);
+
+    // But, the following does compile!
+    consume_path_only_as_path_then_pass_down_as_tt!(clippy::alloc_instead_of_core);
 }
